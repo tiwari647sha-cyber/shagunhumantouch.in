d5224ad05f954648d1c6fa272a0eb86d8f0f3319
tiwari647sha-cyber <tiwari647sha@gmail.com>
2026-02-01 21:52:10 +0530

    Add files via upload

---
+++ Index.tsx
@@ -0,0 +1,27 @@
+import Header from "@/components/Header";
+import HeroSection from "@/components/HeroSection";
+import AboutSection from "@/components/AboutSection";
+import SkillsSection from "@/components/SkillsSection";
+import ServicesSection from "@/components/ServicesSection";
+import PortfolioSection from "@/components/PortfolioSection";
+import ContactSection from "@/components/ContactSection";
+import Footer from "@/components/Footer";
+
+const Index = () => {
+  return (
+    <div className="min-h-screen">
+      <Header />
+      <main>
+        <HeroSection />
+        <AboutSection />
+        <SkillsSection />
+        <ServicesSection />
+        <PortfolioSection />
+        <ContactSection />
+      </main>
+      <Footer />
+    </div>
+  );
+};
+
+export default Index;
